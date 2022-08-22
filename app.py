5430d83efcf798e0541fb6a63fe4787150fd8cc9
Moses Farley <mcfarley@email.wm.edu>
2022-08-22 15:31:15 -0400

    Fix drop down for input & added error msgs

--- app.py
+++ app.py
@@ -18,10 +18,30 @@ def apology(code=400):
     """Render message as an apology to user."""
     return render_template("apology.html"), code
 
+def render_home(alert_msg):
+        titles = connect(os.path.join(path, "TopTen.db"))
+
+        movies = pd.read_sql('SELECT * FROM films', titles)
+
+        ranking_filmIDS = pd.read_sql('SELECT film_id FROM rankings', titles)
+
+        titles.close()
+
+        filmID_list = ranking_filmIDS['film_id'].unique().tolist()
+
+        goof =  movies['id'].isin(filmID_list)
+
+        movies = movies[goof]
+
+        film_list = movies['title'].unique().tolist()
+
+        return render_template("home.html", film_list=film_list, alert_msg=alert_msg)
+
 @app.route("/", methods=["GET", "POST"])
 def home():
-    titles = connect(os.path.join(path, "TopTen.db"))
     if request.method == "POST":
+        titles = connect(os.path.join(path, "TopTen.db"))
+
         film1 = request.form.get("film_one")
         film2 = request.form.get("film_two")
         film3 = request.form.get("film_three")
@@ -34,14 +54,24 @@ def home():
         film10 = request.form.get("film_ten")
         
         user_favs = {1:film1, 2:film2, 3:film3, 4:film4, 5:film5, 6:film6, 7:film7, 8:film8, 9:film9, 10:film10}
+        duplicate_check = set()
+        for values in user_favs.values():
+            if values == '':
+                alert = 1
+                return render_home(alert)
+            duplicate_check.add(values)
+        if len(duplicate_check) < 10:
+            alert = 1
+            return render_home(alert)
         #dict of user film rankings 
-        
         user_id = find_user_id()
         rankings = add_user(user_favs, user_id)
         critic_matches = cosine_similarity(user_id, rankings)
         #for above 3: see helper.py
         
         critics = pd.read_sql('SELECT * FROM critics', titles)
+
+        titles.close()
 
         match_filter = critics['id'].isin(critic_matches[0])
         #basic description of how panda filtering works in helpers.py
@@ -71,23 +101,7 @@ def home():
    
     else:
 
-        movies = pd.read_sql('SELECT * FROM films', titles)
-
-        ranking_filmIDS = pd.read_sql('SELECT film_id FROM rankings', titles)
-
-        titles.close()
-
-        filmID_list = ranking_filmIDS['film_id'].unique().tolist()
-
-        goof =  movies['id'].isin(filmID_list)
-
-        movies = movies[goof]
-
-        film_list = movies['title'].unique().tolist()
-
-        print(len(film_list))
-
-        return render_template("home.html", film_list=film_list)
+        return(render_home(alert_msg=''))
 
         #TO DO
         # 1.DONE write descriptions for helper functions 
